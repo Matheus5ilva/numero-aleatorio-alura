ef9495f45246ef8c46f779a386680335dede3b71
matheus5ilva <matheusxandre4@gmail.com>
2024-05-15 10:18:39 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,56 @@
+let listaNumerosSorteados = [];
+let quantidade = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+exibirMensagemInicial();
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto!');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+}
+
+function exibirTextoNaTela(tag, texto) {
+    document.querySelector(tag).innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', `Você descobriu o número secreto! Você gastou ${tentativas} ${tentativas > 1 ? ' tentativas' : ' tentativa'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        exibirTextoNaTela('h1', 'Errou!');
+        exibirTextoNaTela('p', `O número secreto é ${chute > numeroSecreto ? 'menor' : 'maior'} que o chute!`);
+        tentativas++;
+        limparCampo();
+    }
+
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    tentativas = 1;
+    exibirMensagemInicial();
+    limparCampo();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = Math.floor(Math.random() * (quantidade + 1));
+    if (listaNumerosSorteados.length == quantidade) {
+        listaNumerosSorteados = [];
+    }
+    if (listaNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    }
+    listaNumerosSorteados.push(numeroEscolhido);
+    return numeroEscolhido;
+}
